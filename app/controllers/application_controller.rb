21bf2f7a08e21fb0af68feca1912bfe94f40b9e0
shota_suzuki <seshiruff4214@yahoo.co.jp>
2023-06-05 22:28:11 +0900

    :bug: userとcompanyのログイン後の遷移を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,13 @@ class ApplicationController < ActionController::Base
     #strong parametersを設定し、employee_numberを許可
     devise_parameter_sanitizer.permit(:sign_up, keys: [:employee_number])
   end
+
+  def after_sign_in_path_for(resource)
+    if company_signed_in?
+      users_path(resource)
+    else
+      root_path
+    end
+  end
+
 end
