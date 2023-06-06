ca91d6ec929fffa75557b179e2f2f45a7ecd0f19
shota_suzuki <seshiruff4214@yahoo.co.jp>
2023-06-06 11:04:19 +0900

    :sparkles: user/showに当月勤怠表示・管理者が勤怠を登録・編集・削除できる機能実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,32 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
+    # ログインユーザーの当日のタイムカード情報取得無かったら新規作成
+    if @user.time_cards.find_by(work_day: Date.today).present?
+      @time_card = @user.time_cards.find_by(work_day: Date.today)
+    else
+      @time_card = TimeCard.new
+    end
+
+    # 当月のカレンダー作成
+    if params[:date].present?
+      @display_month = params[:date].to_date
+    else
+      @display_month = Date.today
+    end
+    @start_date = @display_month.beginning_of_month
+    @end_date = @display_month.end_of_month
+
+    # 当月の勤務実績取得
+    @work_records = {}
+    (@start_date..@end_date).each do |day|
+      if @user.time_cards.find_by(work_day: day).present?
+        @work_records.store(day, @user.time_cards.find_by(work_day: day))
+      else
+        @work_records.store(day, nil)
+      end
+    end
   end
 
   def edit
